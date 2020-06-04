6ef5ccff45d281cebaea8df97e837476fcfcd702
Aditya-Narvekar <66407020+Aditya-Narvekar@users.noreply.github.com>
2020-06-04 21:49:58 +0530

    Create file.c

---
+++ file.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+	int a,b=0,i;
+	printf("Enter any number:");
+	scanf("%d",&a);
+	if(a==1)
+		printf("1 is neither prime not composite");
+	else
+	{
+	for(i=1;i<=a;i++)
+	{
+		if(a%i==0)
+		{
+			b++;
+		}
+	}
+	if(b==2)
+	{
+		printf("It is a prime number");
+	}
+	else
+	{
+		printf("It is not a prime number");
+	}
+	}
+}
+
